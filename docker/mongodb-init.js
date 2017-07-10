70c543eb3e667c54dcee9bf3d6d5cc16094c04ea
fcferreira <fcferreira@ubiwhere.com>
2017-07-10 18:41:26 +0100

    Docker installation and documentation update.

--- docker/mongodb-init.js
+++ docker/mongodb-init.js
@@ -1,9 +1,13 @@
-conn = new Mongo();
+// Connect to the database.
+conn = new Mongo('127.0.0.1:' + PORT);
 db = conn.getDB(STORE_COLLECTION);
 
-
+// Create the user.
 db.runCommand({
     createUser: STORE_USER,
     pwd: STORE_PASS,
     roles: ["readWrite", "dbAdmin"]
 });
+
+// Show the details to the user to confirm the addition.
+printjson(db.getUsers())
