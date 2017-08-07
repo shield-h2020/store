0806a9c62220191745eb3735140238285983d82f
fcferreira <fcferreira@ubiwhere.com>
2017-08-07 16:57:30 +0100

    OSM onboarding - wip.

--- src/store.py
+++ src/store.py
@@ -12,16 +12,25 @@ import tempfile
 import yaml
 from eve import Eve
 from flask import abort
-from werkzeug.datastructures import FileStorage, ImmutableMultiDict
+from werkzeug.datastructures import ImmutableMultiDict, FileStorage
 from werkzeug.utils import secure_filename
 
 import log
 import settings as cfg
 import store_errors as err
-from vnsfo import VnsfOrchestratorApi
+from vnsfo import VnsfOrchestratorApi, VnsfOrchestratorOnboardingException
 
 
-def onboard(request):
+def onboard_vnsf(request):
+    """
+    Registers a vNSF into the Store and onboards it with the Orchestrator.
+
+    The SHIELD manifest is checked for integrity and compliance. Metadata is stored for the catalogue and the actual
+    manifest file is stored as binary so it can be provided for attestation purposes (thus ensuring tamper-proofing).
+
+    :param request: the HTTP request data.
+    """
+
     if 'package' not in request.files:
         logger.error('Missing <package> field in POST')
         abort(err.HTTP_412_PRECONDITION_FAILED, err.PKG_MISSING_FILE)
@@ -52,22 +61,27 @@ def onboard(request):
         package.extractall(extracted_package_path)
         package.close()
 
+        # Get SHIELD manifest data.
         manifest_path = os.path.join(extracted_package_path, 'manifest.yaml')
         if not os.path.isfile(manifest_path):
             logger.error("Missing 'manifest.yaml' from package")
             abort(err.HTTP_406_NOT_ACCEPTABLE, err.PKG_NOT_SHIELD)
-        stream = open(manifest_path, 'r')
-        manifest = dict(yaml.load(stream))
-        stream.close()
+        with open(manifest_path, 'r') as stream:
+            manifest = dict(yaml.load(stream))
+            logger.debug(manifest)
 
-        print(manifest)
-
+        # Get the vNSF Descriptor data.
         # TODO Ensure the file name is secure. secure_filename() removes the / characters which isn't nice
-        desciptor_file = manifest['manifest:vnsf']['descriptor']
-        stream = open(os.path.join(extracted_package_path, desciptor_file), 'r')
-        descriptor = stream.read()
-        stream.close()
+        vnfd_file = manifest['manifest:vnsf']['descriptor']
+        vnfd_path = os.path.join(extracted_package_path, vnfd_file)
+        if not os.path.isfile(vnfd_path):
+            logger.error("Missing vNSFD. Expected at '{}'".format(vnfd_file))
+            abort(err.HTTP_406_NOT_ACCEPTABLE, err.PKG_MISSING_VNSFD)
+        with open(vnfd_path, 'r') as stream:
+            vnfd = stream.read()
 
+        # Ensure the SHIELD manifest is stored as a binary file.
+        # NOTE: the file is closed by Eve once stored.
         stream = open(manifest_path, 'rb')
         fs = FileStorage(stream)
         files = request.files.copy()
@@ -80,12 +94,14 @@ def onboard(request):
         package_data['registry'] = {'vendor': "vNSF maker A", 'capabilities': ["some stuff"]}
         package_data['state'] = 'sandboxed'
         package_data['manifest'] = manifest
-        package_data['descriptor'] = descriptor
+        package_data['descriptor'] = vnfd
         request.form = ImmutableMultiDict(package_data)
 
         # Onboard the vNSF with the Orchestrator.
-        vnsfo = VnsfOrchestratorApi(cfg.VNSFO_HOST, cfg.VNSFO_PORT, cfg.VNSFO_API)  # , logger)
-        if not vnsfo.onboard_vnsf(cfg.TENANT_ID, package_absolute_path):
+        try:
+            vnsfo = VnsfOrchestratorApi(cfg.VNSFO_PROTOCOL, cfg.VNSFO_HOST, cfg.VNSFO_PORT, cfg.VNSFO_API)
+            vnsfo.onboard_vnsf(cfg.VNSFO_TENANT_ID, package_absolute_path)
+        except VnsfOrchestratorOnboardingException:
             logger.error('vNSF not onboarded with the Orchestrator')
             abort(err.HTTP_502_BAD_GATEWAY, err.VNSF_NOT_ADDED_TO_CATALOG)
 
@@ -126,7 +142,7 @@ def send_attestation(request, response):
 
 
 app = Eve()
-app.on_pre_POST_vnsfs += onboard
+app.on_pre_POST_vnsfs += onboard_vnsf
 app.on_fetched_item_vnsfs += send_minimal_vnsf_data
 app.on_post_GET_attestation += send_attestation
 
