634a92f16343cdd9de165eaae9063e73345633cf
fcferreira <fcferreira@ubiwhere.com>
2017-10-09 17:36:09 +0100

    Licensing details.

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,30 @@
 # -*- coding: utf-8 -*-
 
+#  Copyright (c) 2017 SHIELD, UBIWHERE
+# ALL RIGHTS RESERVED.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Neither the name of the SHIELD, UBIWHERE nor the names of its
+# contributors may be used to endorse or promote products derived from this
+# software without specific prior written permission.
+#
+# This work has been performed in the framework of the SHIELD project,
+# funded by the European Commission under Grant number 700199 through the
+# Horizon 2020 program. The authors would like to acknowledge the contributions
+# of their colleagues of the SHIELD partner consortium (www.shield-h2020.eu).
+
+
 import os
 
 API_PORT = int(os.environ.get('API_PORT', 5000))
@@ -17,6 +42,9 @@ VNSFO_API = os.environ.get('VNSFO_API', '__missing_vnsfo_api_basepath__')
 
 # NOTE: this shall be removed once AAA is in place.
 VNSFO_TENANT_ID = os.environ.get('VNSFO_TENANT_ID', '__no_tenant_set__')
+
+# CORS for any domain.
+X_DOMAINS = '*'
 
 #
 # Enable reads (GET), inserts (POST) and DELETE for resources/collections
